cd49e50ac2d57baff823c893545b108e1dc4d3a9
NickDeliSchool <nikolaos.deligiannis@students.plymouth.ac.uk>
2024-03-27 03:43:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -6,15 +6,54 @@ if (document.getElementById(LoginForm).style.display == 'block') {
 
     document.getElementById(LoginForm).style.display = 'none';
 
-    document.getElementById(RegisterForm).style.display = 'block';
+    document.getElementById(RegisterForm).style.display = 'block' ;
+
+    document.getElementById(RegisterForm).style.display = 'grid' ;
+
+    document.getElementById(LoginForm).style.zIndex = 0;
+
+    document.getElementById(RegisterForm).style.zIndex = 1;
+
+
+
+  }
+
+  if (document.getElementById(LoginForm).style.display == 'grid') {
+
+    document.getElementById(LoginForm).style.display = 'none';
+
+    document.getElementById(RegisterForm).style.display = 'block' ;
+
+    document.getElementById(RegisterForm).style.display = 'grid' ;
+
+    document.getElementById(LoginForm).style.zIndex = 0;
+
+    document.getElementById(RegisterForm).style.zIndex = 1;
+
+
+
   }
 
   else {
 
     document.getElementById(RegisterForm).style.display = 'none';
 
-    document.getElementById(LoginForm).style.display = 'block';
+    document.getElementById(LoginForm).style.display = 'grid';
+
+    document.getElementById(RegisterForm).style.zIndex = 0;
+
+    document.getElementById(LoginForm).style.zIndex = 1;
+
    }
+
+   
+}
+
+function HideFunction() {
+
+    document.getElementById("Overlay").style.display = "block";
+
+
 }
 
 
