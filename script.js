e39c449bc8ebfff3c42a7858256370f78838612a
NickDeliSchool <nikolaos.deligiannis@students.plymouth.ac.uk>
2024-03-27 03:10:56 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 
 function switchRegister_and_Login(LoginForm,RegisterForm) {
 
-if (document.getElementById(LoginForm).style.display = 'block') {
+if (document.getElementById(LoginForm).style.display == 'block') {
 
-    document.getElementById(LoginForm).style.display = 'none'
+    document.getElementById(LoginForm).style.display = 'none';
 
-    document.getElementById(RegisterForm).style.display = 'block'
+    document.getElementById(RegisterForm).style.display = 'block';
   }
 
-  else 
+  else {
 
-    document.getElementById(RegisterForm).style.display = 'none'
+    document.getElementById(RegisterForm).style.display = 'none';
 
-    document.getElementById(LoginForm).style.display = 'block'
-
+    document.getElementById(LoginForm).style.display = 'block';
+   }
 }
 
 
