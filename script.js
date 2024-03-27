4406ec9526fc96bbc6baa2e26d9a924d22ab86f1
NickDeliSchool <nikolaos.deligiannis@students.plymouth.ac.uk>
2024-03-27 03:01:55 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+
+
+function switchRegister_and_Login(LoginForm,RegisterForm) {
+
+if (document.getElementById(LoginForm).style.display = 'block') {
+
+    document.getElementById(LoginForm).style.display = 'none'
+
+    document.getElementById(RegisterForm).style.display = 'block'
+  }
+
+  else 
+
+    document.getElementById(RegisterForm).style.display = 'none'
+
+    document.getElementById(LoginForm).style.display = 'block'
+
+}
+
+
+
+
