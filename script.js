fb4b21913384650ed9cc8b8c18a961f7db0a4549
NickDeliSchool <nikolaos.deligiannis@students.plymouth.ac.uk>
2024-03-27 05:20:14 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,88 +1,98 @@
 
 
-function switchRegister_and_Login(LoginForm,RegisterForm) {
+    function switchRegister_and_Login(LoginForm,RegisterForm) {
 
-if (document.getElementById(LoginForm).style.display == 'block') {
+    if (document.getElementById(LoginForm).style.display == 'block') {
 
-    document.getElementById(LoginForm).style.display = 'none';
+        document.getElementById(LoginForm).style.display = 'none';
 
-    document.getElementById(RegisterForm).style.display = 'block' ;
+        document.getElementById(RegisterForm).style.display = 'block' ;
 
-    document.getElementById(RegisterForm).style.display = 'grid' ;
+        document.getElementById(RegisterForm).style.display = 'grid' ;
 
-    document.getElementById(LoginForm).style.zIndex = 0;
+        document.getElementById(LoginForm).style.zIndex = 0;
 
-    document.getElementById(RegisterForm).style.zIndex = 1;
-
-
-
-  }
-
-  if (document.getElementById(LoginForm).style.display == 'grid') {
-
-    document.getElementById(LoginForm).style.display = 'none';
-
-    document.getElementById(RegisterForm).style.display = 'block' ;
-
-    document.getElementById(RegisterForm).style.display = 'grid' ;
-
-    document.getElementById(LoginForm).style.zIndex = 0;
-
-    document.getElementById(RegisterForm).style.zIndex = 1;
+        document.getElementById(RegisterForm).style.zIndex = 1;
 
 
 
-  }
+    }
 
-  else {
+    if (document.getElementById(LoginForm).style.display == 'grid') {
 
-    document.getElementById(RegisterForm).style.display = 'none';
+        document.getElementById(LoginForm).style.display = 'none';
 
-    document.getElementById(LoginForm).style.display = 'grid';
+        document.getElementById(RegisterForm).style.display = 'block' ;
 
-    document.getElementById(RegisterForm).style.zIndex = 0;
+        document.getElementById(RegisterForm).style.display = 'grid' ;
 
-    document.getElementById(LoginForm).style.zIndex = 1;
+        document.getElementById(LoginForm).style.zIndex = 0;
 
-   }
-
-   
-}
-
-function HideFunction() {
-
-    document.getElementById("Overlay").style.display = "block";
-
-
-}
-
-function DontHideFunction() {
-
-
-    document.getElementById("Overlay").style.display = "none";
-
-}
-
-function OpenAppointment(){
-    
+        document.getElementById(RegisterForm).style.zIndex = 1;
 
 
 
-}
+    }
 
-function CloseAppointment(){
+    else {
 
+        document.getElementById(RegisterForm).style.display = 'none';
 
+        document.getElementById(LoginForm).style.display = 'grid';
 
+        document.getElementById(RegisterForm).style.zIndex = 0;
 
-}
+        document.getElementById(LoginForm).style.zIndex = 1;
 
-function AddApointment(){
-
-
-
+    }
 
     
-}
+    }
+
+    function CloseVerfication(){
+
+
+        document.getElementById(RegisterForm).style.display = 'none';
+
+
+        document.getElementById(LoginForm).style.display = 'none';
+        
+    }
+
+    function HideFunction() {
+
+        document.getElementById("Overlay").style.display = "block";
+
+
+    }
+
+    function DontHideFunction() {
+
+
+        document.getElementById("Overlay").style.display = "none";
+
+    }
+
+    function OpenAppointment(){
+        
+        document.getElementById("AppointmentForm").style.display = "block";
+
+
+    }
+
+    function CloseAppointment(){
+
+        document.getElementById("AppointmentForm").style.display = "none";
+
+
+    }
+
+    function AddApointment(){
+
+
+
+
+
+    }
 
 
